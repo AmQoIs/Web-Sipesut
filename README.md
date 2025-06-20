b55d015295a9289bfcbefe83d28c08f725baceae
AmQoIs <87815591+AmQoIs@users.noreply.github.com>
2025-06-20 14:11:59 +0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
-# Web-Sipesut
+# SIPESUT
+
+## Sistem Informasi Pengelolaan Surat Inspektorat Jenderal TNI
+
+## Dependency PHP yang digunakan
+
+-   "laravel/framework"
+-   "laravel/breeze"
+
+## Dependency Javascript yang digunakan
+
+-   "alpinejs"
+-   "tailwindcss"
+-   "sweetalert2"
+
+## List Role
+
+-   Admin
+-   Anggota
+
+## ERD dan Use Case
+
+https://drive.google.com/file/d/1-5J3-BprTCxu-loqyPsJcQ4zUBsbULyV/view?usp=sharing
+
+## Panduan Instalasi
+
+Yang dilakukan setelah clone
+
+1. Install composer-dependency pake `composer install`
+2. Install npm package pake `npm install`, ini buat install beberapa package seperti tailwind dan alphine
+3. Jalankan build css nya, pake `npm run dev`
+4. Copy file `.env.example` di root folder terus ganti jadi `.env`. Setting sesuai dengan environment yang digunakan
+5. Setting database, sesuaikan nama databasenya dengan yang ada di `.env`
+6. Migrasikan database, `php artisan migrate`
+7. Jalankan seeder `php artisan db:seed`
+8. Jalankan `php artisan key:generate`
+9. Jalankan project nya `npm run dev` lalu tambah terminal baru dan jalankan `php artisan serve`
---
+++ tailwind.config.js
@@ -0,0 +1,44 @@
+import defaultTheme from "tailwindcss/defaultTheme";
+import forms from "@tailwindcss/forms";
+
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+        "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
+        "./storage/framework/views/*.php",
+        "./resources/views/**/*.blade.php",
+    ],
+
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ["Figtree", ...defaultTheme.fontFamily.sans],
+            },
+            colors: {
+                primary: "#D9603A",
+                gray: {
+                    400: "#828282"
+                },
+                secondary: "#5383e9"
+            }
+        },
+    },
+
+    daisyui: {
+        themes: [
+            {
+                mytheme: {
+                    primary: "#D9603A",
+                    secondary: "#f6d860",
+                    accent: "#37cdbe",
+                    neutral: "#3d4451",
+                    "base-100": "#ffffff",
+                },
+            },
+            "dark",
+            "cupcake",
+        ],
+    },
+
+    plugins: [require("daisyui"), forms],
+};
